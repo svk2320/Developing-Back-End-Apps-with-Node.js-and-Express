ff4a77b7a48b6b2f6eb093df9344184f89382ac9
username <vasanthkumars2025@gmail.com>
2023-03-11 18:09:19 +0530

    added base files

---
+++ router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+      1: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      2: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      3: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+      4: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+      5: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      6: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"Vit Babenco" : "The matchless trilogy by Samuel Beckett is in the first echelon of the everlasting masterpieces – under the mask of feeblemindedness and senility of characters, the greatest wisdom is disguised."} },
+      7: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      8: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+      9: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      10: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} }
+}
+
+module.exports = books;
